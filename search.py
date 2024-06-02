4994907878d66f547a23a7f77e8f15d5e0035ffc
Tyrone Nicholas <tyrone.nicholas@sapi.com>
2024-06-02 17:27:05 +0100

    Faster and unique

--- search.py
+++ search.py
@@ -1,15 +1,28 @@
 import stories
 import places
+import concurrent.futures
 
 
 def search(constituency, topic):
     places_list = places.getPlaces(constituency)
     stories_list = stories.getStories(constituency, topic)
-    # TODO do this in parallel
-    for place in places_list:
-        stories_list.extend(stories.getStories(place, topic))
-    # TODO filter out duplicates
-    return stories_list
+
+    # Function to get stories for a given place
+    def get_stories_for_place(place):
+        return stories.getStories(place, topic)
+
+    # Use ThreadPoolExecutor to parallelize the fetching of stories
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        futures = [
+            executor.submit(get_stories_for_place, place) for place in places_list
+        ]
+        for future in concurrent.futures.as_completed(futures):
+            stories_list.extend(future.result())
+
+    # Filter out duplicates
+    unique_stories = {story.title: story for story in stories_list}.values()
+
+    return list(unique_stories)
 
 
 print(search("Croydon East", "crime"))
