9e3c709b1a72671ea315eace254e2c0fd0aaf9a6
lonehegelian <thelonehegelian@protonmail.com>
2024-06-03 23:18:05 +0100

    add news google as image domain

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["imgs.search.brave.com"],
+    domains: ["imgs.search.brave.com", "news.google.com"],
   },
 };
 
