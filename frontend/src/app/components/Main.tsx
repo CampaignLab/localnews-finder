09237c127c276b4ef2db5cbb8c7982504f8ada21
Tyrone Nicholas <tyrone.nicholas@gmail.com>
2024-06-15 16:21:20 +0100

    Added autocomplete

--- frontend/src/app/components/Main.tsx
+++ frontend/src/app/components/Main.tsx
@@ -3,7 +3,7 @@ import Search from "./Search";
 const Main = () => {
   return (
     <>
-      <div className="flex flex-col overflow-y-hidden justify-center items-center">
+      <div className="flex flex-col justify-center items-center">
         <h1>Local News Finder</h1>
         <div className="divider divider-neutral"></div>
 
--- frontend/src/app/components/Search.tsx
+++ frontend/src/app/components/Search.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import { Article } from "@/types";
 import axios from "axios";
 import Articles from "./Articles";
+import Constituency from "./Constituency";
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
@@ -14,10 +15,6 @@ const Search = () => {
   // BUG! first selection is null
   const handleTopicChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     setSelectedTopic(event.target.value);
-  };
-
-  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSearch(event.target.value);
   };
 
   async function getArticles(
@@ -32,32 +29,11 @@ const Search = () => {
     return response.data;
   }
 
-  function autoComplete() {}
-
   return (
     <>
-      <div className="flex flex-col lg:flex-row justify-normal gap-4 m-4 lg:m-10 w-80 lg:w-full">
-        <label className="input input-bordered flex items-center gap-2">
-          <input
-            type="text"
-            className="grow"
-            placeholder="Find your Town"
-            value={search}
-            onChange={handleSearchChange}
-          />
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 16 16"
-            fill="currentColor"
-            className="w-4 h-4 opacity-70"
-          >
-            <path
-              fillRule="evenodd"
-              d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
-              clipRule="evenodd"
-            />
-          </svg>
-        </label>
+      <div className="border-1 flex flex-col lg:flex-row justify-normal gap-4 m-4 lg:m-10 w-80 lg:w-full">
+        <label className="gap-2">Constituency</label>
+        <Constituency className="grow" onSelect={setSearch} />
 
         <select
           className="select select-bordered w-full max-w-xs"
